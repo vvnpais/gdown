bb6bc80b970f962d03d7bea5f22db3a63a9f2fcf
Vivian <210010034@iitdh.ac.in>
2024-01-23 21:55:38 +0530

    jan23 changes

--- gdrive_download.sh
+++ gdrive_download.sh
@@ -1,2 +1,2 @@
 id=$(echo $1 | sed -rn 's@.*/d/([-_A-Za-z0-9]+)/view.*@\1@p')
-wget -c --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$id -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$id" -O $2 && rm -rf /tmp/cookies.txt
+wget -c --content-disposition "https://drive.usercontent.google.com/download?export=download&id=$id&authuser=0&confirm=t"
